42fdd32a6798ca6699cb31023a4c246e21555cfa
Zhitao (Reid) Zhang <zzt124@cs.washington.edu>
2018-01-03 19:15:30 -0800

    update datastore and handler for vessel-service

--- vessel-service/datastore.go
+++ vessel-service/datastore.go
@@ -4,13 +4,14 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
-// CreateSession creates mongodb instance
-func CreateSession(host string) (*mgo.session, error) {
+// CreateSession creates the main session to our mongodb instance
+func CreateSession(host string) (*mgo.Session, error) {
 	session, err := mgo.Dial(host)
 	if err != nil {
 		return nil, err
 	}
 
 	session.SetMode(mgo.Monotonic, true)
+
 	return session, nil
 }
